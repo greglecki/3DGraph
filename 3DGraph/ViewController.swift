7ba64ddfed77a41c939c966ee7650b55c334cca8
Greg Lecki <greg.lecki@pbit-1001597m.silverlinetools.com>
2015-09-03 17:17:14 +0100

    Initial Commit

---
+++ 3DGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DGraph
+//
+//  Created by Greg Lecki on 03/09/2015.
+//  Copyright (c) 2015 Greg Lecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
